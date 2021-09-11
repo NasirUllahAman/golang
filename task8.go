5f4374d311797b2e269e8388b11b1950423737f1
NasirUllahAman <80219207+NasirUllahAman@users.noreply.github.com>
2021-09-11 10:10:22 +0500

    Add files via upload

---
+++ task8.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int
+	var gender string
+	var height float32
+	var weight int
+	var isemployed bool
+	age = 34
+	gender = "male"
+	height = 78.4
+	weight = 78
+	isemployed = true
+	fmt.Println("age:", age)
+	fmt.Println("gender:", gender)
+	fmt.Println("height:", height)
+	fmt.Println("weight:", weight)
+	fmt.Println("isemplyed:", isemployed)
+}
