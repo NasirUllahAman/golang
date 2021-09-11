33199523673d1bba17defa576f1ee847013f46e8
NasirUllahAman <80219207+NasirUllahAman@users.noreply.github.com>
2021-09-11 10:40:58 +0500

    Add files via upload

---
+++ task9.go
@@ -0,0 +1,21 @@
+/*Write a program in C++ to print the following pattern. Go to the editor
+ Sample Output:
+ xxxxx
+x     x       x        x
+x             x        x
+x          xxxxxxx  xxxxxxx
+x             x        x
+x     x       x        x
+ xxxxx  */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(" xxxxx")
+	fmt.Println("x     x       x       x")
+	fmt.Println("x             x       x")
+	fmt.Println("x          xxxxxxx  xxxxxxx")
+	fmt.Println("x          x          x")
+	fmt.Println("xxxxx")
+}
